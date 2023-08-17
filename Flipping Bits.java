c9d55743b75be489bae5946a99c658964080b889
Jhon Sabogal <JhonSabogal11@outlook.com>
2023-08-16 23:57:00 -0500

    Create Flipping Bits.java

---
+++ Flipping Bits.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'flippingBits' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static long flippingBits(long n) {
+    // Write your code here
+      String result = Long.toBinaryString( n & 0xffffffffL | 0x100000000L ).substring(1);
+ StringBuilder sb = new StringBuilder(result);
+        
+ for(int i=0;i<sb.length();i++){
+ char ch=sb.charAt(i);
+ if(ch=='0'){
+ sb.setCharAt(i, '1');
+ }
+ else{
+ sb.setCharAt(i, '0');
+ }
+ }
+ String str=sb.toString();
+ long dec=Long.parseLong(str,2);
+ return dec;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                long result = Result.flippingBits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
