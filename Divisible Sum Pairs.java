d77ea5861fbe71e7a882ad47b95087b7df1d750e
Jhon Sabogal <JhonSabogal11@outlook.com>
2024-08-19 16:52:59 -0500

    Update Divisible Sum Pairs
    Brackets error

--- Divisible Sum Pairs.java
+++ Divisible Sum Pairs.java
@@ -23,17 +23,17 @@ class Result {
      */
 
     public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
-        int count=0;
-        for (int i = 0; i < ar.size(); i++) {
-            for (int j = i+1; j < ar.size(); j++) {
-                if((ar.get(i)+ar.get(j)) % k == 0){
-                    count++;
-                   
+    int count = 0;
+    for (int i = 0; i < ar.size(); i++) {
+        for (int j = i + 1; j < ar.size(); j++) {
+            if ((ar.get(i) + ar.get(j)) % k == 0) {
+                count++;
             }
         }
-        return count;
-
     }
+    return count;
+}
+
 
 }
 
